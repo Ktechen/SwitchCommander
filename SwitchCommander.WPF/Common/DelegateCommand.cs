35fd571698d91a03bb322a3546bd3948fbeeca8b
Kevin <kevHTW@gmx.de>
2023-09-29 22:49:54 +0200

    Add WPF application to SwitchCommander and implement basic user UI
    This commit introduces a new WPF application into the SwitchCommander solution. The application's MainWindow has basic UI for user information input - email, name1, and name2 with a "Create" button to submit. A MainWindowViewModel is created to bind with the frontend inputs and the command action of the "Create" button. DelegateCommand.cs is added to implement the ICommand interface. The solution file is also updated to include the newly added WPF application project. UserClientService.cs is generated to handle API calls. This setup is to provide a basic GUI to interact with the User model for the next steps of development.

---
+++ SwitchCommander.WPF/Common/DelegateCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SwitchCommander.WPF.Common;
+
+public class DelegateCommand : ICommand
+{
+    private readonly Action _execute;
+    private readonly Func<bool> _canExecute;
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    public DelegateCommand(Action execute, Func<bool> canExecute = null)
+    {
+        _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        _canExecute = canExecute;
+    }
+
+    public bool CanExecute(object? parameter) => _canExecute();
+
+    public void Execute(object? parameter) => _execute();
+}
---
+++ SwitchCommander.WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using SwitchCommander.WebAPI.Client;
+using SwitchCommander.WPF.Common;
+
+namespace SwitchCommander.WPF.ViewModels;
+
+public class MainWindowViewModel : INotifyPropertyChanged
+{
+    private string _email;
+    private string _name;
+    private string _nameTwo;
+    
+    public event PropertyChangedEventHandler PropertyChanged;
+    
+    public string Email 
+    { 
+        get => _email;
+        set
+        {
+            if (value == _email)
+                return;
+            _email = value;
+            OnPropertyChanged(nameof(Email));
+        }
+    }
+
+    public string Name 
+    { 
+        get => _name;
+        set
+        {
+            if (value == _name)
+                return;
+            _name = value;
+            OnPropertyChanged(nameof(Name));
+        }
+    }
+
+    public string NameTwo 
+    { 
+        get => _nameTwo;
+        set
+        {
+            if (value == _nameTwo)
+                return;
+            _nameTwo = value;
+            OnPropertyChanged(nameof(NameTwo));
+        }
+    }
+
+    public ICommand CreateUserCommand { get; }
+
+    public MainWindowViewModel() 
+    {
+        CreateUserCommand = new DelegateCommand(CreateUser, CanCreateUser);
+    }
+
+    private bool CanCreateUser() 
+    {
+        // Return true or false depending on some rule.
+        return true;
+    }
+
+    private void CreateUser() 
+    {
+        // Create user here.
+        var model = new CreateUserRequest
+        {
+            Email = _email,
+            Name = _name
+        };
+    }
+
+    protected virtual void OnPropertyChanged(string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
