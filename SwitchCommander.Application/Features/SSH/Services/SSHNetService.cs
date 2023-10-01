8410373ebae22d9d4e3eccce3def14021d0ba7c5
Kevin <kevHTW@gmx.de>
2023-10-01 02:30:58 +0200

    Add SSH.NET package and implement SSHNetService
    Added the SSH.NET package to the project in order to manage SSH connections directly from our application. Implemented the SSHNetService which currently supports opening an SSH connection and running a basic command. The 'CloseConnection' method is yet to be implemented. SSH related configurations and services folders have been removed as they are no longer needed.

---
+++ SwitchCommander.Application/Features/SSH/Services/SSHNetService.cs
@@ -0,0 +1,22 @@
+﻿using Renci.SshNet;
+
+namespace SwitchCommander.Application.Features.SSH.Services;
+
+public class SSHNetService 
+{
+
+    public async Task OpenConnection(string host, string username, string password)
+    {
+        using var client = new SshClient(host, username, password);
+        client.Connect();
+        var result = client.RunCommand("ls");
+        var resultAsString = result.Result;
+        
+        await Task.CompletedTask;
+    }
+
+    public Task CloseConnection()
+    {
+        throw new NotImplementedException();
+    }
+}
