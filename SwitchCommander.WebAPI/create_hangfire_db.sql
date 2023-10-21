6521aeab6f2e8aeee470946e160748ea48356205
Kevin <kevHTW@gmx.de>
2023-10-21 17:49:24 +0200

    "Update container names and connection strings in docker-compose.yml and appsettings.json files
    These changes are made to standardize the naming convention across the project and enhance the database connectivity by updating connection strings in settings files. Container names for the webapi, mongodb and sqlServer are updated, along with specifying host paths for the MongoDB and SQL Server data volumes. Connection strings are then altered in appsettings.json and appsettings.Development.json to reflect these new container names. Also, a script file 'create_hangfire_db.sql' is added to automate the creation of a new 'Hangfire' database. This will improve overall project maintainability."

---
+++ SwitchCommander.WebAPI/create_hangfire_db.sql
@@ -0,0 +1,14 @@
+﻿USE master;
+GO
+
+-- Check if the database already exists and drop it if it does
+IF EXISTS (SELECT name FROM sys.databases WHERE name = 'Hangfire')
+BEGIN
+    ALTER DATABASE Hangfire
+    SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE Hangfire;
+END
+
+-- Create the Hangfire database
+CREATE DATABASE Hangfire;
+GO
